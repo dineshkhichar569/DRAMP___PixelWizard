8a462b7af91e3b3b0053dce9182578e8427f4895
dineshkhichar569 <dineshkhichar569@email.com>
2024-10-18 21:27:16 +0530

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = " Your Growth"
+    }, 0)
+    setTimeout(() => {
+        text.textContent = " WebDevelopment"
+    }, 4000)
+    setTimeout(() => {
+        text.textContent = " Hackathon"
+    }, 8000)
+    setTimeout(() => {
+        text.textContent = " Freelance"
+    }, 12000)
+}
+
+textLoad();
+
+setInterval(textLoad, 16000);
